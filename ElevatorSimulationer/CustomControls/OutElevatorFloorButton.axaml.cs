9beebfefce5ebdca30aabf4d526e879075bce851
Ouse <2216528769@qq.com>
2025-10-29 16:28:28 +0800

    UI完成，开始设计调度算法

--- ElevatorSimulationer/CustomControls/OutElevatorFloorButton.axaml.cs
+++ ElevatorSimulationer/CustomControls/OutElevatorFloorButton.axaml.cs
@@ -1,59 +1,163 @@
+using System;
+using System.Diagnostics;
 using System.Windows.Input;
 using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
+using Avalonia.Controls.Shapes;
+using Avalonia.Input;
+using Avalonia.Media;
+using ElevatorSimulationer.Misc;
 
-namespace ElevatorSimulationer.CustomControls;
-
-public partial class OutElevatorFloorButton : UserControl
+namespace ElevatorSimulationer.CustomControls
 {
-
-    public static readonly StyledProperty<ICommand> LeftClickCommandProperty =
-     AvaloniaProperty.Register<ElevatorFloorButton, ICommand>(nameof(LeftClickCommand));
-
-    public ICommand LeftClickCommand
+    public partial class OutElevatorFloorButton : UserControl
     {
-        get => GetValue(LeftClickCommandProperty);
-        set => SetValue(LeftClickCommandProperty, value);
-    }
+        private const double DoubleClickTimeoutMs = 300;
 
-    public static readonly StyledProperty<ICommand> LeftDoubleClickCancelCommandProperty =
-        AvaloniaProperty.Register<ElevatorFloorButton, ICommand>(nameof(LeftDoubleClickCancelCommand));
+        private bool _upChecked;
+        private bool _downChecked;
 
-    public ICommand LeftDoubleClickCancelCommand
-    {
-        get => GetValue(LeftDoubleClickCancelCommandProperty);
-        set => SetValue(LeftDoubleClickCancelCommandProperty, value);
-    }
+        private DateTime _lastUpClickTime;
+        private DateTime _lastDownClickTime;
 
-    public static readonly StyledProperty<ICommand> RIghtClickCommandProperty =
- AvaloniaProperty.Register<ElevatorFloorButton, ICommand>(nameof(RightClickCommand));
+        public static readonly StyledProperty<int> FloorProperty =
+            AvaloniaProperty.Register<OutElevatorFloorButton, int>(nameof(Floor), 0);
 
-    public ICommand RightClickCommand
-    {
-        get => GetValue(RIghtClickCommandProperty);
-        set => SetValue(RIghtClickCommandProperty, value);
-    }
+        public int Floor
+        {
+            get => GetValue(FloorProperty);
+            set => SetValue(FloorProperty, value);
+        }
 
-    public static readonly StyledProperty<ICommand> RightDoubleClickCancelCommandProperty =
-        AvaloniaProperty.Register<ElevatorFloorButton, ICommand>(nameof(RightDoubleClickCancelCommand));
+        public static readonly StyledProperty<ICommand> UpClickCommandProperty =
+            AvaloniaProperty.Register<OutElevatorFloorButton, ICommand>(nameof(UpClickCommand));
 
-    public ICommand RightDoubleClickCancelCommand
-    {
-        get => GetValue(RightDoubleClickCancelCommandProperty);
-        set => SetValue(RightDoubleClickCancelCommandProperty, value);
-    }
+        public ICommand UpClickCommand
+        {
+            get => GetValue(UpClickCommandProperty);
+            set => SetValue(UpClickCommandProperty, value);
+        }
 
-    public static readonly StyledProperty<int> FloorProperty =
-    AvaloniaProperty.Register<ElevatorFloorButton, int>(nameof(Floor), 0);
+        public static readonly StyledProperty<ICommand> UpDoubleClickCancelCommandProperty =
+            AvaloniaProperty.Register<OutElevatorFloorButton, ICommand>(nameof(UpDoubleClickCancelCommand));
 
-    public int Floor
-    {
-        get => GetValue(FloorProperty);
-        set => SetValue(FloorProperty, value);
-    }
-    public OutElevatorFloorButton()
-    {
-        InitializeComponent();
+        public ICommand UpDoubleClickCancelCommand
+        {
+            get => GetValue(UpDoubleClickCancelCommandProperty);
+            set => SetValue(UpDoubleClickCancelCommandProperty, value);
+        }
+
+        public static readonly StyledProperty<ICommand> DownClickCommandProperty =
+            AvaloniaProperty.Register<OutElevatorFloorButton, ICommand>(nameof(DownClickCommand));
+
+        public ICommand DownClickCommand
+        {
+            get => GetValue(DownClickCommandProperty);
+            set => SetValue(DownClickCommandProperty, value);
+        }
+
+        public static readonly StyledProperty<ICommand> DownDoubleClickCancelCommandProperty =
+            AvaloniaProperty.Register<OutElevatorFloorButton, ICommand>(nameof(DownDoubleClickCancelCommand));
+
+        public ICommand DownDoubleClickCancelCommand
+        {
+            get => GetValue(DownDoubleClickCancelCommandProperty);
+            set => SetValue(DownDoubleClickCancelCommandProperty, value);
+        }
+
+        public static readonly StyledProperty<bool> UpActiveProperty =
+            AvaloniaProperty.Register<OutElevatorFloorButton, bool>(nameof(UpActive));
+
+        public bool UpActive
+        {
+            get => GetValue(UpActiveProperty);
+            set => SetValue(UpActiveProperty, value);
+        }
+
+        public static readonly StyledProperty<bool> DownActiveProperty =
+            AvaloniaProperty.Register<OutElevatorFloorButton, bool>(nameof(DownActive));
+
+        public bool DownActive
+        {
+            get => GetValue(DownActiveProperty);
+            set => SetValue(DownActiveProperty, value);
+        }
+
+        public OutElevatorFloorButton()
+        {
+            InitializeComponent();
+
+            UpEllipse.PointerPressed += OnUpPressed;
+            DownEllipse.PointerPressed += OnDownPressed;
+        }
+        protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
+        {
+            base.OnPropertyChanged(change);
+
+            // 上行按钮状态同步
+            if (change.Property == UpActiveProperty)
+            {
+                _upChecked = change.GetNewValue<bool>();
+                // 这里可以改颜色，比如 Fill
+                var brush = _upChecked ? Settings.ClickDefaultFill : Settings.DefaultFill;
+                (UpEllipse.Children[0] as Ellipse)!.Fill = brush;
+            }
+
+            // 下行按钮状态同步
+            if (change.Property == DownActiveProperty)
+            {
+                _downChecked = change.GetNewValue<bool>();
+                var brush = _downChecked ? Settings.ClickDefaultFill : Settings.DefaultFill;
+                (DownEllipse.Children[0] as Ellipse)!.Fill = brush;
+            }
+        }
+
+        private void OnUpPressed(object? sender, PointerPressedEventArgs e)
+        {
+            var now = DateTime.Now;
+            var interval = (now - _lastUpClickTime).TotalMilliseconds;
+            _lastUpClickTime = now;
+
+            if (_upChecked && interval < DoubleClickTimeoutMs)
+            {
+                Debug.WriteLine($"触发 {Floor} 楼上行双击取消事件");
+                _upChecked = false;
+                UpActive = false;
+                UpDoubleClickCancelCommand?.Execute(Floor);
+                return;
+            }
+
+            if (!_upChecked)
+            {
+                Debug.WriteLine($"触发 {Floor} 楼上行点击事件");
+                _upChecked = true;
+                UpActive = true;
+                UpClickCommand?.Execute(Floor);
+            }
+        }
+
+        private void OnDownPressed(object? sender, PointerPressedEventArgs e)
+        {
+            var now = DateTime.Now;
+            var interval = (now - _lastDownClickTime).TotalMilliseconds;
+            _lastDownClickTime = now;
+
+            if (_downChecked && interval < DoubleClickTimeoutMs)
+            {
+                Debug.WriteLine($"触发 {Floor} 楼下行双击取消事件");
+                _downChecked = false;
+                DownActive = false;
+                DownDoubleClickCancelCommand?.Execute(Floor);
+                return;
+            }
+
+            if (!_downChecked)
+            {
+                Debug.WriteLine($"触发 {Floor} 楼下行点击事件");
+                _downChecked = true;
+                DownActive = true;
+                DownClickCommand?.Execute(Floor);
+            }
+        }
     }
 }
--- ElevatorSimulationer/ViewModels/OutElevatorUIViewModel.cs
+++ ElevatorSimulationer/ViewModels/OutElevatorUIViewModel.cs
@@ -1,29 +1,75 @@
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using ElevatorSimulationer.Events;
 using ElevatorSimulationer.Misc;
+using ElevatorSimulationer.ViewModels;
 using Microsoft.Extensions.Logging;
+using Prism.Commands;
+using Prism.Events;
+namespace ElevatorSimulationer.ViewModels;
 
-namespace ElevatorSimulationer.ViewModels
+internal class OutElevatorUIViewModel : ViewModelBase
 {
-    internal class OutElevatorUIViewModel:ViewModelBase
-    {
-        private readonly ILogger<OutElevatorUIViewModel> _logger;
-        public ObservableCollection<OutElevatorFloorViewModel> OutElevatorFloorModel { get;private set; } = new ObservableCollection<OutElevatorFloorViewModel>();
+    private readonly ILogger<OutElevatorUIViewModel> _logger;
+    private readonly IEventAggregator _eventAggregator;
 
-        public OutElevatorUIViewModel(ILogger<OutElevatorUIViewModel> logger)
+    public ObservableCollection<OutElevatorFloorViewModel> OutElevatorFloorModel { get; private set; }
+        = new ObservableCollection<OutElevatorFloorViewModel>();
+
+    public DelegateCommand<object> UpClickCommand { get; }
+    public DelegateCommand<object> UpDoubleClickCancelCommand { get; }
+    public DelegateCommand<object> DownClickCommand { get; }
+    public DelegateCommand<object> DownDoubleClickCancelCommand { get; }
+
+    public OutElevatorUIViewModel(ILogger<OutElevatorUIViewModel> logger, IEventAggregator eventAggregator)
+    {
+        _logger = logger;
+        _eventAggregator = eventAggregator;
+
+        foreach (var item in Enumerable.Range(1, Settings.FloorCount))
         {
-            _logger = logger;
-            foreach(var item in Enumerable.Range(1,Settings.FloorCount))
+            OutElevatorFloorModel.Add(new OutElevatorFloorViewModel
             {
-                OutElevatorFloorModel.Add(new OutElevatorFloorViewModel
-                {
-                    Floor = item,
-                });
-            }
+                Floor = item,
+            });
         }
+
+        // 上行点击
+        UpClickCommand = new DelegateCommand<object>(data =>
+        {
+            var floorNum = (int)data;
+            var floor = OutElevatorFloorModel.FirstOrDefault(x => x.Floor == floorNum);
+            if (floor != null) floor.IsUpActived = true;
+            _logger.LogInformation($"上行 {floorNum} 楼按钮点击");
+            _eventAggregator.GetEvent<ElevatorStateChangedEvent>().Publish();
+        });
+
+        UpDoubleClickCancelCommand = new DelegateCommand<object>(data =>
+        {
+            var floorNum = (int)data;
+            var floor = OutElevatorFloorModel.FirstOrDefault(x => x.Floor == floorNum);
+            if (floor != null) floor.IsUpActived = false;
+            _logger.LogInformation($"上行 {floorNum} 楼按钮取消");
+            _eventAggregator.GetEvent<ElevatorStateChangedEvent>().Publish();
+        });
+
+        DownClickCommand = new DelegateCommand<object>(data =>
+        {
+            var floorNum = (int)data;
+            var floor = OutElevatorFloorModel.FirstOrDefault(x => x.Floor == floorNum);
+            if (floor != null) floor.IsDownActived = true;
+            _logger.LogInformation($"下行 {floorNum} 楼按钮点击");
+            _eventAggregator.GetEvent<ElevatorStateChangedEvent>().Publish();
+        });
+
+        DownDoubleClickCancelCommand = new DelegateCommand<object>(data =>
+        {
+            var floorNum = (int)data;
+            var floor = OutElevatorFloorModel.FirstOrDefault(x => x.Floor == floorNum);
+            if (floor != null) floor.IsDownActived = false;
+            _logger.LogInformation($"下行 {floorNum} 楼按钮取消");
+            _eventAggregator.GetEvent<ElevatorStateChangedEvent>().Publish();
+        });
     }
 }
+
